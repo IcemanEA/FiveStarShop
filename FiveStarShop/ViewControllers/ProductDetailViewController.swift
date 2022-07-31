22b9ae68d5f73bfa8a2904213558695d28ce573f
Egor Ledkov <egor@ledkov.org>
2022-08-01 00:44:30 +0300

    Check up to Merge

--- FiveStarShop/ViewControllers/ProductDetailViewController.swift
+++ FiveStarShop/ViewControllers/ProductDetailViewController.swift
@@ -9,20 +9,26 @@ import UIKit
 
 class ProductDetailViewController: UIViewController {
 
+// MARK: - IBOutlets
+    
     @IBOutlet var image: UIImageView!
     @IBOutlet var name: UILabel!
     @IBOutlet var price: UILabel!
     @IBOutlet var productDescription: UITextView!
     
-    let product = DataStore.shared.products.first
+    let product = DataStore.shared.products.first!
+    
+// MARK: - Override methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         productDescription.textContainer.lineFragmentPadding = 0
         
-        image.image = UIImage(named: product?.article ?? "")
-        name.text = product?.name
-        price.text = product?.price.toRubleCurrency()
-        productDescription.text = product?.description
+        let tempImage = UIImage(named: product.article) ?? UIImage(named: "imagePlaceholder.png")
+        image.image = tempImage
+        
+        name.text = product.name
+        price.text = product.price.toRubleCurrency()
+        productDescription.text = product.description
     }
 }
